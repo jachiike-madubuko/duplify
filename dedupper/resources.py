36717e8eaf72c4da4c7eb0426d2bc51f8bb4c143
jachiike-madubuko <jachiike.madubuko@gmail.com>
2018-06-28 22:04:55 -0600

    updated resources to include CRDs in export

--- dedupper/resources.py
+++ dedupper/resources.py
@@ -1,5 +1,7 @@
 from import_export import resources
 from .models import simple, repContact, sfcontact, duplifyTime, dedupTime, uploadTime
+from import_export.fields import Field
+
 
 class DuplifyTimeResource(resources.ModelResource):
     class Meta:
@@ -14,8 +16,33 @@ class UploadTimeResource(resources.ModelResource):
         model = uploadTime
 
 class RepContactResource(resources.ModelResource):
+    closest1_CRD = Field()
+    closest2_CRD = Field()
+    closest3_CRD = Field()
+
     class Meta:
         model = repContact
+
+    def dehydrate_closest1_CRD(self, repContact):
+        if repContact.closest1:
+            return '{}'.format(repContact.closest1.CRD)
+        else:
+            return ''
+
+    def dehydrate_closest2_CRD(self, repContact):
+        if repContact.closest2:
+            return '{}'.format(repContact.closest2.CRD)
+        else:
+            return ''
+
+
+    def dehydrate_closest3_CRD(self, repContact):
+        if repContact.closest3:
+            return '{}'.format(repContact.closest3.CRD)
+        else:
+            return ''
+
+
 
 
 class SFContactResource(resources.ModelResource):
