6bf48d952980465196d471a0a1517f64e2b181ae
WV Jachi <jmadubuko@wealthvest.com>
2018-07-13 09:13:31 -0600

    settings.py changed for heroku deployment

--- dedupper_app/settings.py
+++ dedupper_app/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'dedupper.apps.DedupperConfig',
     'django.contrib.postgres',
