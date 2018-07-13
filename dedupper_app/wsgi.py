97e832a367c453ee022223d51c54cab366afd97d
WV Jachi <jmadubuko@wealthvest.com>
2018-07-13 08:50:50 -0600

    settings.py changed for heroku deployment

--- dedupper_app/wsgi.py
+++ dedupper_app/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dedupper_app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "heroku.settings")
 
 application = get_wsgi_application()
 
