070343d7441b171440653f87ff7a779c1dae0212
jachiike-madubuko <jachiike.madubuko@gmail.com>
2018-07-25 17:31:29 -0600

    production branch merge
    Configured a seperate repo to deploy duplify.

--- dedupper_app/wsgi.py
+++ dedupper_app/wsgi.py
@@ -10,13 +10,7 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-import sys
-from dedupper_app.settings import BASE_DIR
-
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dedupper_app.settings")
 
 application = get_wsgi_application()
-
-application = DjangoWhiteNoise(application)
