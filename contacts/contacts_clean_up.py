1971b0c04f9eee3d5a0689d796676379e93059d3
jachiike-madubuko <jachiike.madubuko@gmail.com>
2018-08-28 12:03:55 -0600

    contact clean up update

--- contacts/contacts_clean_up.py
+++ contacts/contacts_clean_up.py
@@ -1,25 +1,23 @@
 
 import pandas as pd
 from random import randint
+import numpy as np
+import altair as alt
+import seaborn as sns
+import datetime
+usage_map=usage_df=sf_contact_by_type=None
 
+sf_contact_df = pd.read_pickle('contacts/panda_pickles/contacts.pkl')
 
 def contacts_clean_up(threshold, num_records=12):
     record_types = ['WS','BD','P','WV',]
     api_names = ['marketo', 'afmo', 'old shit', 'dumb shit']
     print('create pandas map')
-    df_map  = {
-        api_name :  {
-            record_type :
-                pd.DataFrame({
-                    'contact'+str(i): [i*i for fields in range(12)] for i in range(randint(28,100))
-                })
-            for record_type in record_types
-        }
-        for api_name in api_names
-    }
+    df_map= contacts_using_fields_in_criteria(threshold, num_records)
     api_names = list(df_map.keys())
     record_types = list(df_map[api_names[0]])
     print('create pandas pickle')
+
     for api_name in api_names:
         for record_type in record_types:
             df_map[api_name][record_type].to_pickle(f'contacts/panda_pickles/{api_name}-{record_type}.pkl')
@@ -33,3 +31,54 @@ def get_resampled_field_count(table):
     #{ 'date field' : table.resample('M').count() }
     data =  {}
     return data
+
+def fieldsByThreshold(df,up_percent, low_percent=0):
+    #percentage of the fields usage in SF
+    total_usage_per_col = df.count()/len(df);
+    return total_usage_per_col[ total_usage_per_col.between(low_percent/100,up_percent/100)]
+
+
+def threshold_per_type(max_threshold,
+                       min_num_types):  # input a percent, and a minimun number of records types to be under used by
+    all_fields = list(usage_df.index)
+
+    fields = [i for i in all_fields if
+              len([j for j in list(usage_df.loc[i] <= max_threshold / 100) if j == True]) >= min_num_types]
+    return usage_df.loc[fields]  # all the fields that fall under 'threshold' usage 'cnt' times
+# example -> threshold_per_type(1,12)  all fields that have under 1% usage for 12 or more record types
+
+
+def contact_type_using_field(record_type, field_name):
+    if record_type == 'All':
+        contacts = sf_contact_df
+    else:
+        contacts = sf_contact_by_type[record_type]
+
+    return contacts[contacts[field_name].notnull()]
+
+
+# get contacts using under used fields
+def contacts_using_fields_in_criteria(thresh_percent, num_types):
+    globals()['sf_contact_by_type'] = {i: sf_contact_df[sf_contact_df['RECORD_TYPE_NAME__C'] == i] for i in
+                          sf_contact_df.RECORD_TYPE_NAME__C.unique()}
+
+    # fields usage per record type
+    globals()['usage_map'] = {i: fieldsByThreshold(sf_contact_by_type[i], 101) for i in
+                           sf_contact_df.RECORD_TYPE_NAME__C.unique()}
+    globals()['usage_df'] = pd.DataFrame(usage_map)
+
+    types = list(usage_df.columns)
+    types.append('All')
+    # fields that fit the criteria
+    fields_in_threshold = list(threshold_per_type(thresh_percent, num_types).index)
+    # dictionary containing each field in the criteria and the contacts using that field for each record type
+    return {i: {j: contact_type_using_field(j, i) for j in types} for i in fields_in_threshold}
+
+
+
+
+def date_range_of_contacts(df):
+    earliest = df.resample('M').count()[:1].index.date[0].isoformat()
+    latest = df.resample('M').count()[-1:].index.date[0].isoformat()
+    return f'from {earliest} to {latest}'
+
