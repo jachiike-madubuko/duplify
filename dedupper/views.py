5601ba9433e2609804a0f4fbbddb24a3358f003f
jachiike-madubuko <jachiike.madubuko@gmail.com>
2018-10-10 01:41:10 -0600

    pandas contacts implemented

--- dedupper/views.py
+++ dedupper/views.py
@@ -466,7 +466,7 @@ def db_progress(request):
             try:
                 db_job = q.fetch_job(request.session['rq_job'])
                 if db_job:
-                    msg = 2
+                    msg = 'success'
                     reps , sf= progress.objects.latest().label.split('--$--')
                     print(f'reps:{len(reps)},sf:{len(sf)}, ')
                     pd.read_csv(sf).to_hdf('sf_contact.hdf', 'trill')
