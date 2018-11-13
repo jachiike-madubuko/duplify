1cc5f06cd4717a70890c0de4c1344da8debca52b
jachiike-madubuko <jachiike.madubuko@gmail.com>
2018-11-13 15:40:04 -0700

    fuzzy pandas subset selection improvement

--- dedupper/admin.py
+++ dedupper/admin.py
@@ -5,8 +5,3 @@ from .models import *
 # Register your models here.
 admin.site.register(industry)
 admin.site.register(manufacturer)
-admin.site.register(sfcontact)
-admin.site.register(repContact)
-admin.site.register(dedupTime)
-admin.site.register(duplifyTime)
-admin.site.register(uploadTime)
